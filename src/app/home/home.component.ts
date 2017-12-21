1bf17ccb01a2befd927eecac8dc820dc56527a1a
NoyaSchleien <noyaschleien@gmail.com>
2017-12-21 14:02:51 +0200

    4th draft

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  zoomAnimation:string;
   constructor() { }
 
   ngOnInit() {
   }
-
+  zoom(){
+  this.zoomAnimation="w3-animate-zoom";
+}
 }
