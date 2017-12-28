240f7b452129b7c43b83f9ae34e209ecae2d4ce9
NoyaSchleien <noyaschleien@gmail.com>
2017-12-28 09:13:44 +0200

    14 draft

--- src/app/shared/slides/slides.service.ts
+++ src/app/shared/slides/slides.service.ts
@@ -19,13 +19,13 @@ export class SlidesService {
         "description": "Tel Aviv",
         "slideIndex":0,
         "cameras":[
-          {"name":"c1","top":1,"left":1},{"name":"c2","top":20,"left":20},{"name":"c3","top":40,"left":50},{"name":"c4","top":70,"left":70},{"name":"c5","top":200, "left":400},{"name":"c6","top":350,"left":350} 
+          {"name":"c1","top":2,"left":4},{"name":"c2","top":20,"left":20},{"name":"c3","top":80,"left":50},{"name":"c4","top":70,"left":350},{"name":"c5","top":90, "left":450},{"name":"c6","top":20,"left":90} 
           ],  
         "microphones":[
-          {"name":"m1","top":5,"left":5},{"name":"m2","top":30,"left":15},{"name":"m3","top":65,"left":90},{"name":"m4","top":200, "left":200}
+          {"name":"m1","top":5,"left":150},{"name":"m2","top":30,"left":100},{"name":"m3","top":20,"left":500},{"name":"m4","top":200, "left":200}
         ],
         "areas":[
-          {"name":"a1","top":10,"left":10,"areaCameras":[{"c1"},{"c2"},{"c3"}], "areaMicrophones":[{"m1"},{"m2"}]},{"name":"a2","top":350, "left":400,"areaCameras":[{"c4"},{"c5"},{"c6"}], "areaMicrophones":[{"m3"},{"m4"}]}
+          {"name":"a1","top":50,"left":250,"areaCameras":["c1","c2","c3"], "areaMicrophones":["m1","m2"]},{"name":"a2","top":150, "left":500,"areaCameras":["c4","c5","c6"], "areaMicrophones":["m3","m4"]}
         ]
       },
       {
@@ -33,28 +33,27 @@ export class SlidesService {
         "description": "New York",
         "slideIndex":1,
         "cameras":[
-          {"name":"c1","top":1,"left":1},{"name":"c2","top":20,"left":20},{"name":"c3","top":40,"left":50},{"name":"c4","top":70,"left":70},{"name":"c5","top":200, "left":400},{"name":"c6","top":350,"left":350} 
-        ],  
+          {"name":"c1","top":2,"left":4},{"name":"c2","top":20,"left":20},{"name":"c3","top":80,"left":50},{"name":"c4","top":70,"left":350},{"name":"c5","top":90, "left":450},{"name":"c6","top":20,"left":90} 
+          ],  
         "microphones":[
-          {"name":"m1","top":5,"left":5},{"name":"m2","top":30,"left":15},{"name":"m3","top":65,"left":90},{"name":"m4","top":200, "left":200}
+          {"name":"m1","top":5,"left":150},{"name":"m2","top":30,"left":100},{"name":"m3","top":20,"left":500},{"name":"m4","top":200, "left":200}
         ],
         "areas":[
-          {"name":"a1","top":10,"left":10,"areaCameras":[{"c1"},{"c2"},{"c3"}], "areaMicrophones":[{"m1"},{"m2"}]},{"name":"a2","top":50, "left":50,"areaCameras":[{"c4"},{"c5"},{"c6"}], "areaMicrophones":[{"m3"},{"m4"}]}
+          {"name":"a1","top":50,"left":250,"areaCameras":["c1","c2","c3"], "areaMicrophones":["m1","m2"]},{"name":"a2","top":150, "left":500,"areaCameras":["c4","c5","c6"], "areaMicrophones":["m3","m4"]}
         ]
-
       },
       {
         "location": "../assets/maps/map3.jpg",
         "description": "Honolulu",
         "slideIndex":2,
         "cameras":[
-          {"name":"c1","top":1,"left":1},{"name":"c2","top":20,"left":20},{"name":"c3","top":40,"left":50},{"name":"c4","top":70,"left":70},{"name":"c5","top":200, "left":400},{"name":"c6","top":350,"left":350} 
-        ],  
+          {"name":"c1","top":2,"left":4},{"name":"c2","top":20,"left":20},{"name":"c3","top":80,"left":50},{"name":"c4","top":70,"left":350},{"name":"c5","top":90, "left":450},{"name":"c6","top":20,"left":90} 
+          ],  
         "microphones":[
-          {"name":"m1","top":5,"left":5},{"name":"m2","top":30,"left":15},{"name":"m3","top":65,"left":90},{"name":"m4","top":200, "left":200}
+          {"name":"m1","top":5,"left":150},{"name":"m2","top":30,"left":100},{"name":"m3","top":20,"left":500},{"name":"m4","top":200, "left":200}
         ],
         "areas":[
-          {"name":"a1","top":10,"left":10,"areaCameras":[{"c1"},{"c2"},{"c3"}], "areaMicrophones":[{"m1"},{"m2"}]},{"name":"a2","top":50, "left":50,"areaCameras":[{"c4"},{"c5"},{"c6"}], "areaMicrophones":[{"m3"},{"m4"}]}
+          {"name":"a1","top":50,"left":250,"areaCameras":["c1","c2","c3"], "areaMicrophones":["m1","m2"]},{"name":"a2","top":150, "left":500,"areaCameras":["c4","c5","c6"], "areaMicrophones":["m3","m4"]}
         ]
       },
       {
@@ -62,29 +61,30 @@ export class SlidesService {
         "description": "Paris",
         "slideIndex":3,
         "cameras":[
-          {"name":"c1","top":1,"left":1},{"name":"c2","top":20,"left":20},{"name":"c3","top":40,"left":50},{"name":"c4","top":70,"left":70},{"name":"c5","top":200, "left":400},{"name":"c6","top":350,"left":350} 
-        ],  
+          {"name":"c1","top":2,"left":4},{"name":"c2","top":20,"left":20},{"name":"c3","top":80,"left":50},{"name":"c4","top":70,"left":350},{"name":"c5","top":90, "left":450},{"name":"c6","top":20,"left":90} 
+          ],  
         "microphones":[
-          {"name":"m1","top":5,"left":5},{"name":"m2","top":30,"left":15},{"name":"m3","top":65,"left":90},{"name":"m4","top":200, "left":200}
+          {"name":"m1","top":5,"left":150},{"name":"m2","top":30,"left":100},{"name":"m3","top":20,"left":500},{"name":"m4","top":200, "left":200}
         ],
         "areas":[
-          {"name":"a1","top":10,"left":10,"areaCameras":[{"c1"},{"c2"},{"c3"}], "areaMicrophones":[{"m1"},{"m2"}]},{"name":"a2","top":50, "left":50,"areaCameras":[{"c4"},{"c5"},{"c6"}], "areaMicrophones":[{"m3"},{"m4"}]}
-        ]
-      },
-      {
-        "location": "../assets/maps/map5.jpg",
-        "description": "Venice",
-        "slideIndex":4,
-        "cameras":[
-          {"name":"c1","top":1,"left":1},{"name":"c2","top":20,"left":20},{"name":"c3","top":40,"left":50},{"name":"c4","top":70,"left":70},{"name":"c5","top":200, "left":400},{"name":"c6","top":350,"left":350} 
-        ],  
-        "microphones":[
-          {"name":"m1","top":5,"left":5},{"name":"m2","top":30,"left":15},{"name":"m3","top":65,"left":90},{"name":"m4","top":200, "left":200}
-        ],
-        "areas":[
-          {"name":"a1","top":10,"left":10,"areaCameras":[{"c1"},{"c2"},{"c3"}], "areaMicrophones":[{"m1"},{"m2"}]},{"name":"a2","top":50, "left":50,"areaCameras":[{"c4"},{"c5"},{"c6"}], "areaMicrophones":[{"m3"},{"m4"}]}
+          {"name":"a1","top":50,"left":250,"areaCameras":["c1","c2","c3"], "areaMicrophones":["m1","m2"]},{"name":"a2","top":150, "left":500,"areaCameras":["c4","c5","c6"], "areaMicrophones":["m3","m4"]}
         ]
       }
+      // },
+      // {
+      //   "location": "../assets/maps/map5.jpg",
+      //   "description": "Venice",
+      //   "slideIndex":4,
+      //   "cameras":[
+    //     {"name":"c1","top":2,"left":4},{"name":"c2","top":20,"left":20},{"name":"c3","top":80,"left":50},{"name":"c4","top":70,"left":350},{"name":"c5","top":90, "left":450},{"name":"c6","top":20,"left":90} 
+    //   ],  
+    // "microphones":[
+    //   {"name":"m1","top":5,"left":150},{"name":"m2","top":30,"left":100},{"name":"m3","top":20,"left":500},{"name":"m4","top":200, "left":200}
+    // ],
+    // "areas":[
+    //   {"name":"a1","top":50,"left":250,"areaCameras":["c1","c2","c3"], "areaMicrophones":["m1","m2"]},{"name":"a2","top":150, "left":500,"areaCameras":["c4","c5","c6"], "areaMicrophones":["m3","m4"]}
+    // ]
+      // }
     ]
   }
   }
